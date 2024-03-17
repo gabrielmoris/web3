a734fd73facfee5a945c64cffb959ad37ed86e39
gabriel <gabrielcmoris@gmail.com>
2024-03-17 20:24:27 +0100

    create and retrieve the data from NFT cannister and show in frontend

--- nft-opend/src/nft/nft.mo
+++ nft-opend/src/nft/nft.mo
@@ -1,5 +1,25 @@
 import Debug "mo:base/Debug";
+import Principal "mo:base/Principal";
 
-actor NFT{
-    Debug.print("It works")
+// https://internetcomputer.org/docs/current/motoko/main/actor-classes/
+// actor classes allows to create cannisters programmatically
+// We need to initialize different, now only with dfx deploy. It need some args.
+actor class NFT (name: Text, owner: Principal, content: [Nat8]) {
+
+    let itemName = name;
+    let nftOwner = owner;
+    let imageBytes = content;
+
+    public query func getName(): async Text {
+        return itemName;
+    };
+
+    public query func getOwner(): async Principal {
+        return nftOwner;
+    };
+
+    public query func getAsset(): async [Nat8] {
+        return imageBytes;
+    }
+   
 }
--- nft-opend/src/opend_assets/src/components/Item.jsx
+++ nft-opend/src/opend_assets/src/components/Item.jsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import { HttpAgent, Actor } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
+import { Principal } from "@dfinity/principal";
 
-function Item() {
+function Item({ id }) {
+  const [name, setName] = useState("");
+  const [owner, setOwner] = useState("");
+  const [img, setImg] = useState("");
+
+  const principalId = Principal.fromText(id);
+  const localHost = "http://localhost:8080/";
+  const httpAgent = new HttpAgent({ host: localHost });
+
+  async function loadNFT() {
+    const NFTActor = await Actor.createActor(idlFactory, {
+      agent: httpAgent,
+      canisterId: principalId,
+    });
+
+    const NFTName = await NFTActor.getName();
+    const NFTOwner = await NFTActor.getOwner();
+    const imageData = await NFTActor.getAsset();
+    const imgContent = new Uint8Array(imageData);
+    const image = URL.createObjectURL(new Blob([imgContent.buffer], { type: "image/png" }));
+    setName(NFTName);
+    setOwner(NFTOwner.toText());
+    setImg(image);
+  }
+
+  useEffect(() => {
+    loadNFT();
+  }, []);
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
-        <img
-          className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
-        />
+        <img className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img" src={img} />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {name}
+            <span className="purple-text"></span>
           </h2>
-          <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
-          </p>
+          <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">Owner: {owner}</p>
         </div>
       </div>
     </div>
